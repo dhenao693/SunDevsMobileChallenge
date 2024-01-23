a76f8d3d225734913279029c2286973e40129057
Daniel Henao <dhenao693@gmail.com>
2024-01-23 15:48:00 -0500

    Fill register form init

--- src/main/java/tasks/register/RegisterUser.java
+++ src/main/java/tasks/register/RegisterUser.java
@@ -1,20 +1,43 @@
 package tasks.register;
 
+import lombok.AllArgsConstructor;
+import models.User;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
 
-import static userinterface.HomePage.BTN_ALLOW_GET_LOCATION;
-import static userinterface.HomePage.MENU_OPTION;
+import java.util.List;
+import java.util.Map;
 
+import static userinterface.HomePage.MENU;
+import static userinterface.LoginPage.BTN_REGISTER;
+import static userinterface.MenuOptionsPage.BTN_PROFILE;
+import static userinterface.RegisterForm.*;
+import static utility.ConvertMapToModel.convertMapToUser;
+
+@AllArgsConstructor
 public class RegisterUser implements Task {
+    private List<Map<String, String>> users;
+
     @Override
     public <T extends Actor> void performAs(T actor) {
-        actor.attemptsTo(Click.on(BTN_ALLOW_GET_LOCATION.of(MENU_OPTION)));
+        User user = convertMapToUser(users.get(0));
+        actor.attemptsTo(Click.on(MENU));
+        actor.attemptsTo(Click.on(BTN_PROFILE));
+        actor.attemptsTo(Click.on(BTN_REGISTER));
+        actor.attemptsTo(Enter.theValue(user.getName()).into(LBL_NAME));
+        actor.attemptsTo(Enter.theValue(user.getLastName()).into(LBL_LASTNAME));
+        actor.attemptsTo(Enter.theValue(user.getEmail()).into(LBL_EMAIL));
+        actor.attemptsTo(Enter.theValue(user.getEmail()).into(LBL_RETRY_EMAIL));
+        actor.attemptsTo(Enter.theValue(user.getPhone()).into(LBL_PHONE));
+        actor.attemptsTo(Enter.theValue(user.getAddress()).into(LBL_ADDRESS));
+
+        System.out.println("Hola");
     }
 
-    public static RegisterUser inApp(){
-        return Tasks.instrumented(RegisterUser.class);
+    public static RegisterUser inApp(List<Map<String, String>> users) {
+        return Tasks.instrumented(RegisterUser.class, users);
     }
 }
--- src/main/java/userinterface/RegisterForm.java
+++ src/main/java/userinterface/RegisterForm.java
@@ -11,15 +11,15 @@ public class RegisterForm {
     public static final Target LBL_NAME = Target.the("")
             .located(xpath("//android.widget.ScrollView/android.widget.EditText[1]"));
     public static final Target LBL_LASTNAME = Target.the("")
-            .located(accessibilityId("//android.widget.ScrollView/android.widget.EditText[2]"));
+            .located(xpath("//android.widget.ScrollView/android.widget.EditText[2]"));
     public static final Target LBL_EMAIL = Target.the("")
-            .located(accessibilityId("//android.widget.ScrollView/android.widget.EditText[3]"));
+            .located(xpath("//android.widget.ScrollView/android.widget.EditText[3]"));
     public static final Target LBL_RETRY_EMAIL = Target.the("")
-            .located(accessibilityId("//android.widget.ScrollView/android.widget.EditText[4]"));
+            .located(xpath("//android.widget.ScrollView/android.widget.EditText[4]"));
     public static final Target LBL_PHONE = Target.the("")
-            .located(accessibilityId("//android.widget.ScrollView/android.widget.EditText[5]"));
+            .located(xpath("//android.widget.ScrollView/android.widget.EditText[5]"));
     public static final Target LBL_ADDRESS = Target.the("")
-            .located(accessibilityId("//android.widget.ScrollView/android.widget.EditText[5]"));
+            .located(xpath("//android.widget.ScrollView/android.widget.EditText[5]"));
     public static final Target LST_GENDER = Target.the("")
             .located(accessibilityId("sdadsad"));
 
